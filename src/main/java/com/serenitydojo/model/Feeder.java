84c3bf879623f283e4d9c29703abb0babfe6018b
Mina(Elnaz) Khorsand Mahand <elnazkhorsand@gmail.com>
2021-03-12 19:44:35 +0100

    Nested Conditionals

--- src/main/java/com/serenitydojo/model/Feeder.java
+++ src/main/java/com/serenitydojo/model/Feeder.java
@@ -2,7 +2,14 @@ package com.serenitydojo.model;
 
 public class Feeder {
 
-    public String feed(String animal) {
-        return "Tuna";
+    public String feeds(String animal, boolean isPremium) {
+        if(animal.equals("Cat")) {
+            return (isPremium) ? "Salmon" : "Tuna";
+        }
+        else if (animal.equals("Hamster")){
+            return "Cabbage";
+        }
+        else
+            return "Bone";
     }
 }
--- src/test/java/com/serenitydojo/WhenFeedingTheAnimals.java
+++ src/test/java/com/serenitydojo/WhenFeedingTheAnimals.java
@@ -7,10 +7,34 @@ import org.junit.Test;
 public class WhenFeedingTheAnimals {
 
     @Test
-    public void shouldFeedTheAnimalWithTheRightFood(){
+    public void shouldFeedTheCatWithTuna(){
         Feeder feeder = new Feeder();
 
-        String food = feeder.feed("Cat");
+        String food = feeder.feeds("Cat", false);
         Assert.assertEquals("Tuna", food);
+    }
+
+    @Test
+    public void shouldFeedTheHamsterWithCabbage(){
+        Feeder feeder = new Feeder();
+
+        String food = feeder.feeds("Hamster", true);
+        Assert.assertEquals("Cabbage", food);
+    }
+
+    @Test
+    public void shouldFeedTheDogWithBone(){
+        Feeder feeder = new Feeder();
+
+        String food = feeder.feeds("Dog", true);
+        Assert.assertEquals("Bone", food);
+    }
+
+    @Test
+    public void shouldFeedPremiumCatPremiumFood(){
+        Feeder feeder = new Feeder();
+
+        String food = feeder.feeds("Cat", true);
+        Assert.assertEquals("Salmon", food);
     }
 }
