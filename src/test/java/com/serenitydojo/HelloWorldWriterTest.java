b78fae20617f746f9aa2a0a7834f5b4f8ecc9b82
Mina(Elnaz) Khorsand Mahand <elnazkhorsand@gmail.com>
2021-03-14 14:08:15 +0100

    Array - The most basic collection type in Java

--- src/test/java/com/serenitydojo/HelloWorldWriterTest.java
+++ src/test/java/com/serenitydojo/HelloWorldWriterTest.java
@@ -9,4 +9,18 @@ public class HelloWorldWriterTest {
         HelloWorldWriter writer = new HelloWorldWriter();
         writer.writeHelloWorld();
     }
+
+
+    @Test
+    public void workingWithTheMostBasicCollectionsArrayInJava(){
+        String[] colors = {"Red","Blue", "Green"};
+
+        String[] moreColors = new String[3];
+
+        moreColors[0] = "Yellow";
+        moreColors[1] = "Pink";
+
+        System.out.println("The first color is: " + moreColors[0]);
+        System.out.println("The second color is: " + moreColors[1]);
+    }
 }
